cbf2ba6055740addc4744b581a21d50529eecc98
Joschka <joschka@kintscher.me>
2013-11-27 02:43:45 +0100

    More descriptive assertions

--- tests/units.js
+++ tests/units.js
@@ -1,11 +1,14 @@
-test("hello test", function() {
+module('Units - Dummy Tests', {
+});
+
+test("it compares strings with integers", function() {
   ok(1 == "1", "Passed!");
 });
 
 test("it breaks in IE<9", function() {
   if (window.attachEvent && !window.addEventListener) {
-    ok(false, "IE<9 breaks.");
+    ok(false, "This breaks in IE<9");
   } else {
-    ok(true, "Everything else works fine");
+    ok(true, "All other browsers work fine");
   }
 });
